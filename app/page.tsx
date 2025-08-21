88de832b478f8bb3822eeb9399c38739f62a9c74
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-21 09:59:26 +0700

    check reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import StandardizeName from "@/utils/StandlizeName";
-
 export default function Home() {
   return (
     <>
         <div>
           <h1>KK</h1>
-          <h2>Subheading</h2>
         </div>
     </>
   );
