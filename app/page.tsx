930c047507283d62ff8f56cf45843f3f7c930377
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-21 10:46:00 +0700

    add ul li

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,11 @@ export default function Home() {
     <>
         <div>
           <p>SOME FUNCTION {StandardizeName("Some Name")}, Here is main change version 2</p>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
         </div>
     </>
   );
