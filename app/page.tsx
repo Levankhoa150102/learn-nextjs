d8b16ff4397393fdf42c7a1118c38f20d6ff115f
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-21 16:27:50 +0700

    fix: bug on header

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
           <p>SOME FUNCTION {StandardizeName("Some Name")}, Here is main change version 2</p>
           <p>Test Github Action</p>
           <p>Do action to fix bug on main</p>
+          <p>Fix quick bug in hear</p>
         </div>
     </>
   );
