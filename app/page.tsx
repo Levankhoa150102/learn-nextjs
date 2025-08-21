6ba406e7988a83c4e8d0fa1a11cc0f81ec452757
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-21 16:09:46 +0700

    fix: quick fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
         <div>
           <p>SOME FUNCTION {StandardizeName("Some Name")}, Here is main change version 2</p>
           <p>Test Github Action</p>
+          <p>Do action to fix bug on main</p>
         </div>
     </>
   );
