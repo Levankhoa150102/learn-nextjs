bd488c36d6c530f02268be9f11036520a1b3d241
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-21 10:39:18 +0700

    main change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
         <div>
-          <p>SOME FUNCTION {StandardizeName("Some Name")} by merge</p>
+          <p>SOME FUNCTION {StandardizeName("Some Name")}, Here is main change</p>
         </div>
     </>
   );
