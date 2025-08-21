336b6e4880c8b5cdccc4788a8acc182524e7f507
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-21 15:53:24 +0700

    chore: delete some unnecessary text

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ export default function Home() {
     <>
         <div>
           <p>SOME FUNCTION {StandardizeName("Some Name")}, Here is main change version 2</p>
-          <p>Test Github Action</p>
-          <p>Add from testing branch</p>
         </div>
     </>
   );
