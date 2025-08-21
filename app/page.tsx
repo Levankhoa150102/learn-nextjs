f94b7b42a53137db9df7b5221c8c41c56463ba40
Le Van Khoa <88327899+Levankhoa150102@users.noreply.github.com>
2025-08-21 10:47:53 +0700

    add 2 item for list

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ export default function Home() {
             <li>Item 1</li>
             <li>Item 2</li>
             <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
           </ul>
         </div>
     </>
